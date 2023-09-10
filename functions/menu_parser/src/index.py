cc4789d7b1e90e9c7e404bebe154880314602666
Boris Fritscher <boris@fritscher.ch>
2023-09-10 14:42:14 +0200

    test new cli deploy

--- functions/menu_parser/src/index.py
+++ functions/menu_parser/src/index.py
@@ -60,20 +60,6 @@ def extract_text(file_path):
     return menus
 
 
-"""
-  'req' variable has:
-    'headers' - object with request headers
-    'payload' - request body data as a string
-    'variables' - object with function variables
-
-  'res' variable has:
-    'send(text, status)' - function to return text response. Status code defaults to 200
-    'json(obj, status)' - function to return JSON response. Status code defaults to 200
-
-  If an error is thrown, a response with code 500 will be returned.
-"""
-
-
 def main(context):
     client = Client()
 
